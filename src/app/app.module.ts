1a67805b3f49e33137b2e67722df012998ede6dd
Den Stryzhov <den.stryzhov@gmail.com>
2019-08-24 02:12:39 +0300

    Add Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { AlertModule } from 'ngx-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { InventoryComponent } from './inventory/inventory.component';
     BrowserModule,
     AppRoutingModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    AlertModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
