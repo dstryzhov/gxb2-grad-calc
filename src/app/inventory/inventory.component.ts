0f504fa00811e3cff344004ec113d2987fb9c9e2
Den Stryzhov <den.stryzhov@gmail.com>
2019-08-24 21:38:37 +0300

    Add inventory display & edit

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {Girl} from '../models/girl';
 import {ImportService} from '../service/import.service';
+import {faPlus, faMinus} from '@fortawesome/free-solid-svg-icons';
+import {stars} from '../enums/star';
 
 @Component({
   selector: 'app-inventory',
@@ -9,8 +11,11 @@ import {ImportService} from '../service/import.service';
 })
 export class InventoryComponent implements OnInit {
 
+  private iconPlus = faPlus;
+  private iconMinus = faMinus;
+
   private girls: Girl[];
-  private inventory: {[key: string]: number};
+  private inventory: {[key: string]: number} = {};
 
   constructor(private importService: ImportService ) { }
 
@@ -18,4 +23,22 @@ export class InventoryComponent implements OnInit {
     this.girls = this.importService.importGirls();
   }
 
+  getInvCount(id: string) {
+    return this.inventory[id] || 0;
+  }
+
+  remInv(id: string) {
+    if (!this.inventory[id]) {
+      return;
+    }
+    this.inventory[id] = this.inventory[id] - 1;
+  }
+
+  addInv(id: string) {
+    this.inventory[id] = (this.inventory[id] || 0) + 1;
+  }
+
+  getDisplayGirls(): Girl[] {
+    return this.girls.filter(girl => girl.grade !== stars[6]);
+  }
 }
