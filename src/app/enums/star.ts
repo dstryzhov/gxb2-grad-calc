6746d696b56b5f895e5b339fd0192d30687d67d9
Den Stryzhov <den.stryzhov@gmail.com>
2019-08-26 01:09:34 +0300

    Display available graduations

--- src/app/enums/star.ts
+++ src/app/enums/star.ts
@@ -5,17 +5,19 @@ import {Styles} from '@fortawesome/fontawesome-svg-core';
 export class Star {
   icon: IconDefinition[];
   styles: Styles;
+  sortOrder: number;
 
 
-  constructor(icon: IconDefinition[], styles: Styles) {
+  constructor(sortOrder: number, icon: IconDefinition[], styles: Styles) {
     this.icon = icon;
     this.styles = styles;
+    this.sortOrder = sortOrder;
   }
 }
 
 export const stars = {
-  3: new Star([faStar, faStar, faStar], {color: 'gold'}),
-  4: new Star([faStar, faStar, faStar, faStar], {color: 'gold'}),
-  5: new Star([faStar, faStar, faStar, faStar, faStar], {color: 'gold'}),
-  6: new Star([faStar], {color: 'maroon'}),
+  3: new Star(3, [faStar, faStar, faStar], {color: 'gold'}),
+  4: new Star(2, [faStar, faStar, faStar, faStar], {color: 'gold'}),
+  5: new Star(1, [faStar, faStar, faStar, faStar, faStar], {color: 'gold'}),
+  6: new Star(0, [faStar], {color: 'maroon'}),
 };
--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Girl} from '../models/girl';
 import {ImportService} from '../service/import.service';
-import {faPlus, faMinus} from '@fortawesome/free-solid-svg-icons';
 import {stars} from '../enums/star';
+import {SixStarGirl} from '../models/six.star.girl';
+import {InventoryEntry} from '../models/inventory.entry';
+import {FiveStarGirl} from '../models/five.star.girl';
 
 @Component({
   selector: 'app-inventory',
@@ -10,35 +12,103 @@ import {stars} from '../enums/star';
   styleUrls: ['./inventory.component.less']
 })
 export class InventoryComponent implements OnInit {
-
-  private iconPlus = faPlus;
-  private iconMinus = faMinus;
-
   private girls: Girl[];
-  private inventory: {[key: string]: number} = {};
+  private inventory: InventoryEntry[] = [];
+  private gradCards: { [key: string]: InventoryEntry[] } = {};
 
-  constructor(private importService: ImportService ) { }
+  constructor(private importService: ImportService) {
+  }
 
   ngOnInit() {
     this.girls = this.importService.importGirls();
   }
 
-  getInvCount(id: string) {
-    return this.inventory[id] || 0;
+  getInvCount(id: string): number {
+    return this.inventory.filter(entry => entry.girl.id === id).length;
   }
 
-  remInv(id: string) {
-    if (!this.inventory[id]) {
-      return;
-    }
-    this.inventory[id] = this.inventory[id] - 1;
+  remInv(id: string): void {
+    this.inventory.splice(this.inventory.findIndex(entry => entry.girl.id === id), 1);
+    this.prepGradCards();
   }
 
-  addInv(id: string) {
-    this.inventory[id] = (this.inventory[id] || 0) + 1;
+  addInv(id: string): void {
+    const entry = new InventoryEntry(this.girls.find(g => g.id === id));
+    this.inventory.push(entry);
+    this.prepGradCards();
   }
 
   getDisplayGirls(): Girl[] {
     return this.girls.filter(girl => girl.grade !== stars[6]);
+  }
+
+  getGrads(): Girl[] {
+    return this.girls.filter(g => g instanceof FiveStarGirl || g instanceof SixStarGirl)
+      .sort((a, b) => a.grade.sortOrder - b.grade.sortOrder)
+      .filter(girl => this.hasCopiesInInventory(girl as SixStarGirl | FiveStarGirl));
+  }
+
+  private hasCopiesInInventory(girl: SixStarGirl | FiveStarGirl) {
+    return this.inventory.some(entry => entry.girl.id === girl.previousForm);
+  }
+
+  getAvailGradCount(): number {
+    return 0;
+  }
+
+  pickFromInv(toPick: string, targetId: string): InventoryEntry {
+    const avail = this.inventory.filter(entry => entry.girl.id === toPick)
+      .filter(entry => !entry.picked);
+    if (!avail.length) {
+      // no available girls to pick
+      // todo placeholder
+      return null;
+    }
+
+    avail[0].picked = true;
+
+    this.gradCards[targetId].push(avail[0]);
+
+    return avail[0];
+  }
+
+  prepGradCards() {
+    for (const girl of this.girls) {
+      if (!this.gradCards[girl.id]) {
+        this.gradCards[girl.id] = [];
+      }
+
+      if (girl instanceof SixStarGirl || girl instanceof FiveStarGirl) {
+        const picked = this.pickNcopies(girl.id, girl.previousForm, girl.requiredSelfQ);
+        if (picked !== girl.requiredSelfQ) {
+          // reserve food only when have enough copies
+        }
+        this.pickNcopies(girl.id, girl.requiredFood, girl.requiredFoodQ);
+      }
+    }
+  }
+
+  private pickNcopies(mainId, copyId, n) {
+    let copiesPicked = this.getCopiesPicked(mainId, copyId);
+
+    while (copiesPicked < n) {
+      const picked = this.pickFromInv(copyId, mainId);
+      if (picked) {
+        copiesPicked += 1;
+      } else {
+        // no girls in the inventory
+        break;
+      }
+    }
+
+    return copiesPicked;
+  }
+
+  private getCopiesPicked(girlId: string, previousForm): number {
+    return this.gradCards[girlId].filter(g => g.girl.id === previousForm).length;
+  }
+
+  getGradCards(girl: Girl): InventoryEntry[] {
+    return this.gradCards[girl.id] || [];
   }
 }
