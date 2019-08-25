1d11576588dbb90c95ff4a56173efa369ae3f950
Den Stryzhov <den.stryzhov@gmail.com>
2019-08-26 00:54:18 +0300

    Update 5* 6* girls & their models

--- src/app/models/six.star.girl.ts
+++ src/app/models/six.star.girl.ts
@@ -3,16 +3,16 @@ import {stars} from '../enums/star';
 import {factions} from '../enums/faction';
 
 export class SixStarGirl extends Girl {
-  private static REQUIRED_SELF_Q = 2;
-  private static REQUIRED_FOOD_Q = 3;
+  readonly requiredSelfQ = 2;
+  readonly requiredFoodQ = 1;
 
-  private fiveStarForm: string;
-  private requiredFood: string;
+  previousForm: string;
+  requiredFood: string;
 
 
   constructor(json: any) {
     super(json.id, json.name, factions[json.faction], stars[6]);
-    this.fiveStarForm = json.self5star;
+    this.previousForm = json.previousForm;
     this.requiredFood = json.requiredFood;
   }
 }
